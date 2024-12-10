65731a05e0e221fc7a3201c027dc833de428abb8
Raoul Rocco Riigov <rariig@ttu.ee>
2024-12-10 14:30:13 +0200

    Moved Application.java to correct path so PlayController does work now in Postman

--- backend/src/main/java/com/Application.java
+++ backend/src/main/java/com/Application.java
@@ -1,4 +1,4 @@
-package com.company; // Use your actual package name
+package com; // Use your actual package name
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
