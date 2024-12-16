e3896f6d1a672b892402844050a80188ecb99576
Raoul Rocco Riigov <rariig@ttu.ee>
2024-12-16 12:41:38 +0200

    Delete button functionality is working. Confirmation should be added before deletion & refresh the list after deletion.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import YourPlays from './YourPlays'; // Replace with your actual component for displaying the list
-import EditPlay from './EditPlay';
+import YourPlays from './Plays/YourPlays'; // Replace with your actual component for displaying the list
+import EditPlay from './Plays/EditPlay';
 
 const App = () => {
   return (
